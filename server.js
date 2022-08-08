4b17b410bf364b78004b23bb6d8cb65c291dfc47
denise-alvarez <denisealvarez3@gmail.com>
2022-08-08 14:23:12 -0700

    update files

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const htmlRoutes = require("./routes/htmlRoutes");
+const htmlRoutes = require("./Develop/routes/htmlRoutes");
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 
-app.use("/", htmlroutes);
-app.use("/", apiroutes);
+app.use("/", htmlRoutes);
+app.use("/", apiRoutes);
 
 app.listen(PORT, () => console.log('Listening on PORT: ${PORT}'))
